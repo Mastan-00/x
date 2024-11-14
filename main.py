52b89eed50c3664fe77842da56c7c9d8b46e4c68
Mastan-00 <146148676+Mastan-00@users.noreply.github.com>
2024-11-15 00:05:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# To begin:
+
+# Take an input from the user for the total balance to be payed.
+# Take an input from the user for minimum payment amount.
+# Take an input from the user for how many payment periods they have until the deadline.
+# Calculations:
+
+# Calculate the monthly payment needed to fully pay off a balance with the allotted time frame.
+# Prompt the user to input the monthly payment amounts.
+# If the payment amount is too small, change to the minimum payment amount.
+# If the payment is larger than the remaining balance, change to the remaining balance.
+
+
+
+# userPayment = checkOverPayment(checkMinimumPayment(checkPositiveFloat(checkFloat(input(f"Please make your {x}st payment:\n")))))
